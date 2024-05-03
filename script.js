aa3aec28f61010c54baa983d0cb45123bc458bc8
ptmp25 <ptmaiphuong91@gmail.com>
2024-05-03 23:26:30 +0700

    feat: Update calculator theme to dark mode and add backspace functionality

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ const clear_btn = document.getElementById('clear');
 const input = document.getElementById('input1');
 const output = document.getElementById('output');
 const equal_btn = document.getElementById('equal');
+const dot = document.getElementById('dot');
+const back = document.getElementById('back');
 var check = 1;
 
 clear_btn.addEventListener('click', () => {
     check = 1;
     input.value = '';
     output.value = '';
+});
+
+back.addEventListener('click', () => {
+    input.value = input.value.slice(0, -1);
 });
 
 function refresh(){
@@ -31,10 +37,19 @@ for (let number of numbers) {
     });
 }
 
+dot.addEventListener('click', () => {
+    if (input.value.includes('.')) {
+        console.log("You can't add more than one dot");
+        alert("You can't add more than one dot");
+        return;
+    }
+    input.value += '.';
+});
+
 for (let operator of operators_btn) {
     operator.addEventListener('click', () => {
         if (check == 0) {
-            output.value = input.value + operator.textContent;
+            output.value = input.value + ' ' + operator.textContent + ' ';
             input.value = '';
             check = 1;
             return;
@@ -45,10 +60,10 @@ for (let operator of operators_btn) {
             return;
         }
         if (input.value === '' && output.value !== ''){
-            output.value = output.value.slice(0, -1) + operator.textContent;
+            output.value = output.value.slice(0, -3) + ' ' + operator.textContent + ' ';
             return;
         }
-        output.value += input.value + operator.textContent;
+        output.value += input.value + ' ' + operator.textContent + ' ';
         input.value = '';
 
     });
@@ -65,30 +80,10 @@ equal_btn.addEventListener('click', () => {
     input.value = eval(output.value);
     check = 0;
 });
+
 // target the button using the data attribute we added earlier
 const checkbox = document.getElementById('modeToggle');
 checkbox.addEventListener("change", () => {
     const newTheme = checkbox.checked ? "dark" : "light";
     document.querySelector("html").setAttribute("data-theme", newTheme);
-});
-
-button.addEventListener("change", () => {
-  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
-
-  // update the button text
-  const newCta = newTheme === "dark" ? "Change to light theme" : "Change to dark theme";
-  button.innerText = newCta;  
-
-  // use an aria-label if you are omitting text on the button
-  // and using sun/moon icons, for example
-  button.setAttribute("aria-label", newCta);
-
-  // update theme attribute on HTML to switch theme in CSS
-  document.querySelector("html").setAttribute("data-theme", newTheme);
-
-  // update in local storage
-  localStorage.setItem("theme", newTheme);
-
-  // update the currentThemeSetting in memory
-  currentThemeSetting = newTheme;
 });
