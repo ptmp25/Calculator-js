0ed42d139bb1e341619a94770c3774d10a2137a9
ptmp25 <ptmaiphuong91@gmail.com>
2024-05-03 19:24:04 +0700

    Add dark mode toggle functionality and update theme attribute on HTML

--- script.js
+++ script.js
@@ -65,14 +65,30 @@ equal_btn.addEventListener('click', () => {
     input.value = eval(output.value);
     check = 0;
 });
+// target the button using the data attribute we added earlier
+const checkbox = document.getElementById('modeToggle');
+checkbox.addEventListener("change", () => {
+    const newTheme = checkbox.checked ? "dark" : "light";
+    document.querySelector("html").setAttribute("data-theme", newTheme);
+});
 
-const modeToggle = document.getElementById('modeToggle');
-const body = document.body;
-modeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark');
-    if (body.classList.contains('dark')) {
-        modeToggle.textContent = 'Light Mode';
-    } else {
-        modeToggle.textContent = 'Dark Mode';
-    }
+button.addEventListener("change", () => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  // update the button text
+  const newCta = newTheme === "dark" ? "Change to light theme" : "Change to dark theme";
+  button.innerText = newCta;  
+
+  // use an aria-label if you are omitting text on the button
+  // and using sun/moon icons, for example
+  button.setAttribute("aria-label", newCta);
+
+  // update theme attribute on HTML to switch theme in CSS
+  document.querySelector("html").setAttribute("data-theme", newTheme);
+
+  // update in local storage
+  localStorage.setItem("theme", newTheme);
+
+  // update the currentThemeSetting in memory
+  currentThemeSetting = newTheme;
 });
